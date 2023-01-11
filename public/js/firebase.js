dedb87f15157c764fd782957e723eedd7a7ebbf3
meniaclol <arijuslaurin@gmail.com>
2023-01-11 09:53:42 +0100

    retitled wrapper class name

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,12 +1,9 @@
-// db.collection("flokk").add({
-//     flokknavn: "",
-//     serieinndelig: "",
-//     buemerke: "",
-//     eier: ""
-// })
-// .then((docRef) => {
-//     console.log("Document written with ID: ", docRef.id);
-// })
-// .catch((error) => {
-//     console.error("Error adding document: ", error);
-// });
+db.collection("reinsdyr").add(
+    {"Navn": "Guovdageainnut", "Flokk_tilhørighet": "Guovdageainnu", "Fødselsdato": "18. november, 2019", "Serienummer": "G1-2019", "id": "181119567"}
+)
+.then((docRef) => {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch((error) => {
+    console.error("Error adding document: ", error);
+});
