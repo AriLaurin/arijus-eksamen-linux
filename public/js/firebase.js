ce2c0f639b99c5d2991c4282d2d48512bdf354ef
meniaclol <arijuslaurin@gmail.com>
2023-01-11 03:00:56 +0100

    Updated HTML titles

---
+++ public/js/firebase.js
@@ -0,0 +1,12 @@
+// db.collection("flokk").add({
+//     flokknavn: "",
+//     serieinndelig: "",
+//     buemerke: "",
+//     eier: ""
+// })
+// .then((docRef) => {
+//     console.log("Document written with ID: ", docRef.id);
+// })
+// .catch((error) => {
+//     console.error("Error adding document: ", error);
+// });
